7bab345346c15147fca20fe1f9d6336eca89c24c
Franco Loto <franco.loto1995@gmail.com>
2023-10-07 15:30:57 -0300

    feat: added dark mode

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,9 +1,24 @@
-
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState("dark")
+
+  function toggleDarkMode() {
+    if (darkMode === "dark") {
+      setDarkMode("light");
+    } else {
+      setDarkMode("dark");
+    }
+  }
+
   return (
-    <div className="text-blue-600">
-      <h1>React App</h1>
+    <div className={`${darkMode  === "dark" ? "dark" : "light"} min-h-screen flex flex-col`}>
+      <div className="bg-zinc-100 dark:bg-gray-900 flex-auto text-gray-900 dark:text-white">
+        <h1>React App</h1>
+        <button onClick={toggleDarkMode}>
+          <span>Dark Mode</span>
+        </button>
+      </div>
     </div>
   );
 }
